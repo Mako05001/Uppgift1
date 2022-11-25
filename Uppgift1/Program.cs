530d6245278e26c6454e960e2b83d75a8e9c3ab6
Maksymilian kozlowski <mako05001@utb.vaxjo.se>
2022-11-25 10:56:10 +0100

    Add project files.

---
+++ Uppgift1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Tors tid ?");
+double T = double.Parse(Console.ReadLine());
+Console.Write("Mors tid ?");
+double M = double.Parse(Console.ReadLine());
+double m = 40;
+double k = 0;
+double v = 0;
+double l = 1 / T;
+double r = 1 / M;
+
+while (k + v < m)
+{
+    k = k + l;
+
+    v = v + r;
+}
+
+
+int L = (int)Math.Round(k);
+int V = (int)Math.Round(v);
+if (L + V == 41)
+{
+    L--;
+    V--;
+}
+Console.Write("Svar: Tor:" + L + "  ");
+Console.Write("Mor:" + V);
+
+
+
+
+
